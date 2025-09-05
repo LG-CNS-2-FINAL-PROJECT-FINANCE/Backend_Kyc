b0a4f6454ab55d37ccf872b0606c1db43655a297
dohy0103 <kdohy0103@gmail.com>
2025-09-05 10:16:26 +0900

    Update: 0.1.9

--- src/main/java/com/ddiring/Backend_Kyc/integration/apick/ApickClient.java
+++ src/main/java/com/ddiring/Backend_Kyc/integration/apick/ApickClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -14,8 +14,8 @@ public interface ApickClient {
     @PostMapping(value = "/rest/identi_card/1", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     Map<String, Object> verifyResidentId(
             @RequestHeader("CL_AUTH_KEY") String authKey,
-            @RequestPart("name") String name,
-            @RequestPart("rrn1") String rrn1,
-            @RequestPart("rrn2") String rrn2,
-            @RequestPart("date") String issueDate);
+            @RequestParam("name") String name,
+            @RequestParam("rrn1") String rrn1,
+            @RequestParam("rrn2") String rrn2,
+            @RequestParam("date") String issueDate);
 }
